d38ecd8f35a7b82ec7b3ae342b59176bba66d89c
Kevin Hang <herezy2006@gmail.com>
2023-06-21 17:41:23 +0200

    added cars in threejs + debugging the wheels

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,6 +1,10 @@
+import { useBox } from "@react-three/cannon";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useRaycastVehicle } from "@react-three/cannon";
+import { WheelDebug } from "./WheelDebug";
+import { useWheels }  from "./useWheels";
 
 export function Car() {
   let mesh = useLoader(
@@ -8,12 +12,47 @@ export function Car() {
     process.env.PUBLIC_URL + "/models/car2.glb"
   ).scene;
 
+  const position = [-1.5, 0.5, 3];
+  const width = 0.15;
+  const height = 0.07;
+  const wheelRadius = 0.05;
+  const front = 0.15;
+
+  const chassisBodyArgs = [ width, height, front * 2 ];
+  const [ chassisBody, chassisApi ] = useBox(() => ({
+    mass: 150,
+    args: chassisBodyArgs,
+    position,
+  }), useRef(null));
+
+  const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
+
+  const [vehicle, vehicleApi] = useRaycastVehicle(
+    () => ({
+      chassisBody,
+      wheelInfos,
+      wheels,
+    }),
+    useRef(null),
+  );
+
   useEffect(() => {
     mesh.scale.set(0.0012, 0.0012, 0.0012);
-    mesh.children[0].position.set(-360, -18, -67);
+    mesh.children[0].position.set(-365, -18, -67);
   }, [mesh]);
 
   return (
-    <primitive object={mesh} rotation-y={Math.PI} />
+
+    <group ref={vehicle} name="vehicule">
+      <mesh ref={chassisBody} >
+        <meshBasicMaterial transparent={true} opacity={0.3} />
+        <boxGeometry args={chassisBodyArgs} />
+      </mesh>
+
+      <WheelDebug wheelRef={wheels[0]} radius={wheelRadius} />
+      <WheelDebug wheelRef={wheels[1]} radius={wheelRadius} />
+      <WheelDebug wheelRef={wheels[2]} radius={wheelRadius} />
+      <WheelDebug wheelRef={wheels[3]} radius={wheelRadius} />
+    </group>
   )
 }
--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,3 +1,4 @@
+import { usePlane } from "@react-three/cannon";
 import { MeshReflectorMaterial } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
@@ -5,6 +6,13 @@ import { BufferAttribute } from "three";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 export function Ground() {
+
+  const [ref] = usePlane(() => ({
+    type: 'Static',
+    rotation: [-Math.PI / 2, 0, 0]}),
+    useRef(null)
+  );
+
   const grid = useLoader(
     TextureLoader,
     process.env.PUBLIC_URL + "/textures/grid.png"
