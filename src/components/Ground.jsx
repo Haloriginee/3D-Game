ddc435c39e1a6e56dfe12a1d683fda63a65af247
Kevin Hang <herezy2006@gmail.com>
2023-06-21 15:24:05 +0200

    added ground model + car model

---
+++ src/components/Ground.jsx
@@ -0,0 +1,71 @@
+import { MeshReflectorMaterial } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { BufferAttribute } from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
+export function Ground() {
+  const grid = useLoader(
+    TextureLoader,
+    process.env.PUBLIC_URL + "/textures/grid.png"
+  );
+
+  const map = useLoader(
+    TextureLoader,
+    process.env.PUBLIC_URL + "/textures/ground.png"
+  );
+
+  const alpha = useLoader(
+    TextureLoader,
+    process.env.PUBLIC_URL + "/textures/alpha.png"
+  );
+
+  useEffect(() => {
+    grid.anisotropy = 16;
+  }, [grid])
+
+  const meshRef = useRef(null);
+
+  useEffect(() => {
+    var uvs = meshRef.current.geometry.attributes.uv.array;
+    meshRef.current.geometry.setAttribute("uv2", new BufferAttribute(uvs, 2));
+
+  }, [meshRef.current]);
+
+
+  return (
+    <>
+      <mesh
+        ref={meshRef}
+        position={[-2.285, -0.015, -1.325]}
+        rotation-x={-Math.PI * 0.5}
+        rotation-z={-0.079}
+      >
+        <circleGeometry args={[6.12, 50]} />
+        <MeshReflectorMaterial
+          map={map}
+          alpha={alpha}
+          transparent={true}
+          color={[0.5, 0.5, 0.5]}
+          envMapIntensity={0.35}
+          metalness={0.05}
+          roughness={0.4}
+          dithering={true}
+          blur={[1024, 512]} // Blur ground reflections (width, heigt), 0 skips blur
+          mixBlur={3} // How much blur mixes with surface roughness (default = 1)
+          mixStrength={30} // Strength of the reflections
+          mixContrast={1} // Contrast of the reflections
+          resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality, slower
+          mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+          depthScale={0} // Scale the depth factor (0 = no depth, default = 0)
+          minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+          maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+          depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount [bl
+          debug={0}
+          reflectorOffset={0.02} // Offsets the virtual camera that projects the reflection. Useful when the reflective
+        />
+      </mesh>
+
+    </>
+  );
+}
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,6 +1,8 @@
 import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Suspense } from "react";
 import { Track } from "./Track";
+import { Ground } from "./Ground";
+import { Car } from "./Car";
 
 export function Scene() {
   return (
@@ -12,6 +14,8 @@ export function Scene() {
       <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
       <OrbitControls target={[-2.64, -0.71, 0.03]} />
       <Track/>
+      <Ground/>
+      <Car/>
     </Suspense>
   )
 }
