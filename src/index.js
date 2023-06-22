22c8c4f3f1f597634d5379c88d259a6d1c138e6e
Kevin Hang <herezy2006@gmail.com>
2023-06-22 12:49:59 +0200

    added instructions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import { createRoot } from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import { Scene } from "./components/Scene";
 import { Physics } from "@react-three/cannon";
+import { Html } from "@react-three/drei"
+
 
 createRoot(document.getElementById("root")).render(
+
+  <>
+
+
+
+    <div className="instructions">
+      <h1>
+        -Z, Q, S, D to move around
+        <br />
+        -E to Thirdperson
+        <br />
+        -R to Respawn
+      </h1>
+    </div>
+
   <Canvas>
     <Physics
       gravity={[0, -2.6, 0]}
@@ -13,4 +30,5 @@ createRoot(document.getElementById("root")).render(
       <Scene/>
     </Physics>
   </Canvas>
+  </>
 );
